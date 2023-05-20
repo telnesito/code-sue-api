4aab023a05fc21d4d1ab27bef57a0e41f0d281b8
Carlos <carlosj_1703@hotmail.com>
2023-05-20 12:32:04 -0400

    puertos

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ import loginRoutes from "./routes/loginRoutes.js";
 import proyectosRoutes from "./routes/proyectosRoutes.js";
 
 const app = express()
+const port = process.env.PORT || 8000
 
-app.listen(8000)
+app.listen(port)
 app.use(cors())
 app.use(express.json())
-
+console.log(`Server on port ${port}`)
 
 app.use('/usuarios', loginRoutes)
 app.use('/proyectos', proyectosRoutes)
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import app from "./app.js";
 
 const main = () => {
   app.listen(app.get('port'))
-  console.log(`Server on port ${8000}`)
 }
 
 main()
