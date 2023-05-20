e899ddb133c55a92fe0b406da9bb404b8689bf7a
Carlos <carlosj_1703@hotmail.com>
2023-05-20 12:45:43 -0400

    dbsett

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 import pkg from 'pg'
 
 const config = {
-  user: 'postgres',
-  host: 'localhost',
-  password: '27728122',
-  database: 'codesue'
+  user: 'carlosternera',
+  host: 'dpg-chkfgsjhp8uej74a82a0-a',
+  password: 'X0unXRwnJeM02sOT0dkWq91AKHXQlye3',
+  database: 'codesuedb'
 }
 
 export const pool = new pkg.Pool(config)
