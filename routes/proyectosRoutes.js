4dfe1710bce6b13680996a6f8bfe5a5c868dae59
Telnesito <carlosj_1703@hotmail.com>
2023-05-27 22:49:19 -0400

    fix

--- routes/proyectosRoutes.js
+++ routes/proyectosRoutes.js
@@ -6,6 +6,8 @@ const proyectosRoutes = Router()
 proyectosRoutes.post('/crear-proyecto', metodosProjects.createProyecto)
 proyectosRoutes.get('/ver-proyectos', metodosProjects.getProyetos)
 proyectosRoutes.delete('/eliminar-proyecto', metodosProjects.deleteProyectos)
+proyectosRoutes.put('/actualizar-proyecto', metodosProjects.updateProyecto)
+
 
 
 export default proyectosRoutes
